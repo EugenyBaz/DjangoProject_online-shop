c6deb71c1bcc63d1d3e9e9673defabfa76a32fc8
Bazavod <eugeny.bazavod@list.ru>
2025-05-10 15:28:16 +0300

    create models and migrates ones

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name= 'Наименование продукта')
+    description = models.TextField(verbose_name= 'Описание продукта')
+    photo = models.ImageField(upload_to='catalog/foto', blank=True, null=True)
+    category = models.ForeignKey("Category", on_delete= models.SET_NULL, verbose_name='Категория', null=True, related_name='products')
+    price = models.IntegerField(verbose_name= 'Цена за покупку')
+    created_at = models.DateTimeField(verbose_name= 'Дата создания')
+    updated_at = models.DateTimeField(verbose_name= 'Дата последнего изменения')
+
+    class Meta():
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['name', 'category']
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование категории')
+    description = models.TextField(verbose_name='Описание категории')
+
+    class Meta():
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
