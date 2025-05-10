a10c59a80c93181a0bf09b05c1a2583a683f7a83
Bazavod <eugeny.bazavod@list.ru>
2025-05-10 23:36:54 +0300

    create category and product

--- catalog/models.py
+++ catalog/models.py
@@ -4,10 +4,12 @@ class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name= 'Наименование продукта')
     description = models.TextField(verbose_name= 'Описание продукта')
     photo = models.ImageField(upload_to='catalog/foto', blank=True, null=True)
-    category = models.ForeignKey("Category", on_delete= models.SET_NULL, verbose_name='Категория', null=True, related_name='products')
-    price = models.IntegerField(verbose_name= 'Цена за покупку')
-    created_at = models.DateTimeField(verbose_name= 'Дата создания')
-    updated_at = models.DateTimeField(verbose_name= 'Дата последнего изменения')
+    category = models.ForeignKey("Category", on_delete= models.CASCADE, verbose_name='Категория', null=True, related_name='products')
+    price = models.DecimalField(max_digits= 10, decimal_places= 2, verbose_name= 'Цена за покупку')
+    created_at = models.DateTimeField(auto_now_add = True, verbose_name= 'Дата создания', null=True)
+    updated_at = models.DateTimeField(auto_now = True, verbose_name= 'Дата последнего изменения', null=True)
+
+
 
     class Meta():
         verbose_name = 'Продукт'
