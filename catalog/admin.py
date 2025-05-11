47c010651359fcb9be234176db9ab2118ed1a88a
Bazavod <eugeny.bazavod@list.ru>
2025-05-11 18:21:22 +0300

    create fixture

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,14 +4,12 @@ from catalog.models import Product, Category
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("id","name")
+    list_display = ("id", "name")
     search_fields = ("name", "description")
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id","name", "price", "category")
+    list_display = ("id", "name", "price", "category")
     list_filter = ("category",)
     search_fields = ("name", "description")
-
-
-
