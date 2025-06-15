40e8316c97d7dd6afaf020008f8c10397e997df5
Bazavod <eugeny.bazavod@list.ru>
2025-06-15 12:12:50 +0300

    add CACHE_ENABLED

--- config/settings.py
+++ config/settings.py
@@ -165,9 +165,13 @@ SERVER_EMAIL = EMAIL_HOST_USER
 
 LOGIN_URL = "users:login"
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
+
+CACHE_ENABLED = True
+
+if CACHE_ENABLED:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+            'LOCATION': 'redis://127.0.0.1:6379/1',
+        }
     }
-}
